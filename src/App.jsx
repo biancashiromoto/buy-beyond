5125c774fa216efae233ddad69a61d1ae416d288
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-12 20:15:53 -0300

    feat: Import Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Header from './components/Header/Header';
+
 function App() {
 
   return (
     <div>
+      <Header />
     </div>
   )
 }
