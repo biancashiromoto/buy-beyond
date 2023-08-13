65765d3305889578b10e7db6f0eee2e967a8b73d
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-13 14:15:04 -0300

    feat: Create StepsContainer and import StepCards

--- src/components/StepsContainer/StepsContainer.jsx
+++ src/components/StepsContainer/StepsContainer.jsx
@@ -4,76 +4,59 @@ import number02 from '../../assets/images/numbers/number02.png';
 import number03 from '../../assets/images/numbers/number03.png';
 import number04 from '../../assets/images/numbers/number04.png';
 import number05 from '../../assets/images/numbers/number05.png';
+import { useEffect, useState } from 'react';
+import { onlineShopSteps, personalShopperSteps } from '../../helpers/info';
+import StepCard from '../StepCard/StepCard';
 
 const StepsContainer = () => {
+  const images = [number01, number02, number03, number04, number05];
+  const [isOnlineShopping, setIsOnlineShopping] = useState(true);
+  const [steps, setSteps] = useState([]);
+
+  useEffect(() => {
+    if (isOnlineShopping) {
+      setSteps(onlineShopSteps.pt_br.content);
+    } else {
+      setSteps(personalShopperSteps.pt_br.content);
+    }
+  }, [isOnlineShopping]);
+
   return (
-    <div className='card-container d-flex flex-column'>
-      <div
-        className="card flex-row align-items-center justify-content-center"
-      >
-        <img
-          src={number01}
-          className="number-img"
-          alt="Number 01"
-          style={{width: '15vw'}}
-        />
-        <div className="card-body">
-          <p className="card-text">Você envia sua compra online para o nosso endereço</p>
-        </div>
+    <>
+      <div className='online-shopping d-flex align-items-center justify-content-center'>
+        <label className={isOnlineShopping && 'active'}>
+          Online shopping
+          <input
+            className='visually-hidden'
+            type='radio'
+            name='shopping-choice'
+            checked={isOnlineShopping}
+            onChange={() => setIsOnlineShopping(true)}
+          />
+        </label>
+        <label className={!isOnlineShopping && 'active'}>
+          Personal shopper
+          <input
+            className='visually-hidden'
+            type='radio'
+            name='shopping-choice'
+            checked={!isOnlineShopping}
+            onChange={() => setIsOnlineShopping(false)}
+          />
+        </label>
       </div>
-      <div
-        className="card  flex-row align-items-center justify-content-center"
-      >
-        <img
-          src={number02}
-          className="number-img"
-          alt="Number 02"
-          style={{width: '15vw'}}
-        />
-        <div className="card-body">
-          <p className="card-text">Nossa equipe revisa seu produto para garantir a integridade dele</p>
-        </div>
+      <div className='card-container d-flex flex-column'>
+        {steps && steps.map((step, index) => {
+          return (
+          <StepCard
+            imgSrc={images[index]}
+            key={index}
+            text={step}
+          />
+        )
+        })}
       </div>
-      <div
-        className="card  flex-row align-items-center justify-content-center"
-      >
-        <img
-          src={number03}
-          className="card-img-top number-img"
-          alt="Number 03"
-          style={{width: '15vw'}}
-        />
-        <div className="card-body">
-          <p className="card-text">O frete é calculado com base no peso da compra</p>
-        </div>
-      </div>
-      <div
-        className="card  flex-row align-items-center justify-content-center"
-      >
-        <img
-          src={number04}
-          className="card-img-top number-img"
-          alt="Number 04"
-          style={{width: '15vw'}}
-        />
-        <div className="card-body">
-          <p className="card-text">Seu produto é embalado e enviado de forma segura</p>
-        </div>
-      </div>
-      <div
-        className="card  flex-row align-items-center justify-content-center"
-      >
-        <img
-          src={number05}
-          className="card-img-top number-img"
-          alt="Number 04"
-          style={{width: '15vw'}}
-        />
-        <div className="card-body">
-          <p className="card-text">Você recebe suas compras no conforto da sua casa!</p>
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
 
