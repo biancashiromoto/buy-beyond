50ab55b05859fade5a8d512940d2514bc986b885
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-13 14:14:24 -0300

    chore: Create info.js file to store text info

---
+++ src/helpers/info.js
@@ -0,0 +1,40 @@
+export const onlineShopSteps = {
+  pt_br: {
+    content: [
+      'Você envia sua compra online para o nosso endereço',
+      'Nossa equipe revisa seu produto para garantir a integridade dele',
+      'O frete é calculado com base no peso da compra',
+      'Seu produto é embalado e enviado de forma segura',
+      'Você recebe suas compras no conforto da sua casa!',]
+  },
+  en: {
+    content: [
+      'You send your online purchase to our address',
+      'Our team reviews your product to ensure its integrity',
+      'Shipping costs are calculated based on the product\'s weight',
+      'Your product is carefully packaged and shipped securely',
+      'Enjoy the convenience of having your purchases delivered to your home!'
+    ]
+  }
+};
+
+export const personalShopperSteps = {
+  pt_br: {
+    content: [
+      'Entre em contato e nos conte o que você procura',
+      'Nossa equipe vai até a loja e faz a compra pra você',
+      'O frete é calculado com base no peso da compra',
+      'Seu produto é embalado e enviado de forma segura',
+      'Você recebe suas compras no conforto da sua casa!',
+    ]
+  },
+  en: {
+    content: [
+      'Contact us and tell us what you\'re looking for',
+      'Our team drives to the store and shops for you',
+      'Shipping costs are calculated based on the product\'s weight',
+      'Your product is carefully packaged and shipped securely',
+      'Enjoy the convenience of having your purchases delivered to your home!'
+    ]
+  }
+}
