c6c88f65c9074747b2e383df09897275b38c15fc
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-13 20:13:59 -0300

    chore: Change logo import

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import logo02 from '../../assets/images/logo02.png';
+import logo01 from '../../assets/images/logo01.png';
 
 const Header = () => {
   return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          <img className='logo-header-image' src={logo02} style={{width: '15vw'}}/>
+          <img className='logo-header-image' src={logo01} style={{width: '15vw'}}/>
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -14,9 +14,6 @@ const Header = () => {
           <ul className="navbar-nav">
             <li className="nav-item">
               <a className="nav-link" href="#about-us">Sobre nós</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">FAQ</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#footer">
