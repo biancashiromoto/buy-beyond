4955c6f0270656209d377a8dc6998f43af469d44
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-13 12:57:43 -0300

    feat: Import StepsContainer in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Header from './components/Header/Header';
 import Carousel from './components/Carousel/Carousel';
+import StepsContainer from './components/StepsContainer/StepsContainer';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header />
       <Carousel />
+      <StepsContainer />
     </div>
   )
 }
