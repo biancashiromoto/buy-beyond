a4c953386410bd2b0e01ec55d5b70226faca497f
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-22 20:05:21 -0300

    style: Remove inline styling for logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          <img className='logo-header-image' src={logo01} style={{width: '15vw'}} aria-hidden='true'/>
+          <img className='logo-header-image' src={logo01} aria-hidden='true'/>
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
