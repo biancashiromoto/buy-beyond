776ddd47fb217f1353d591d9c5e972a11e4127b7
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-29 18:40:06 -0300

    feat: Add brands

--- src/helpers/brands.js
+++ src/helpers/brands.js
@@ -4,15 +4,13 @@ import calvinKlein from '../assets/brands/calvinklein.png';
 import dior from '../assets/brands/dior.png';
 import goPro from '../assets/brands/gopro.png';
 import haagenDazs from '../assets/brands/haagendazs.png';
-// import calvinKlein from '../assets/brands/calvinklein.png';
-// import calvinKlein from '../assets/brands/calvinklein.png';
-// import calvinKlein from '../assets/brands/calvinklein.png';
-// import calvinKlein from '../assets/brands/calvinklein.png';
-// import calvinKlein from '../assets/brands/calvinklein.png';
-// import calvinKlein from '../assets/brands/calvinklein.png';
-// import calvinKlein from '../assets/brands/calvinklein.png';
-// import calvinKlein from '../assets/brands/calvinklein.png';
-// import calvinKlein from '../assets/brands/calvinklein.png';
+import hm from '../assets/brands/hm.png';
+import mac from '../assets/brands/mac.png';
+import nike from '../assets/brands/nike.png';
+import oreo from '../assets/brands/oreo.png';
+import rayban from '../assets/brands/rayban.png';
+import samsung from '../assets/brands/samsung.png';
+import tommyhilfiger from '../assets/brands/tommyhilfiger.png';
 
 export const brands = [
   {
@@ -45,28 +43,44 @@ export const brands = [
     logo: haagenDazs,
     link: 'https://www.icecream.com/us/en/brands/haagen-dazs'
   },
-  // {
-  //   name: 'Calvin Klein',
-  //   logo: calvinKlein,
-  //   link: 'https://www.calvinklein.com/'
-  // },
-  // {
-  //   name: 'Calvin Klein',
-  //   logo: calvinKlein,
-  //   link: 'https://www.calvinklein.com/'
-  // },
-  // 'Apple',
-  // 'Calvin Klein',
-  // 'Dior',
-  // 'GoPro',
-  // 'Häagen-Dazs',
-  // 'H&M',
-  // 'MAC',
-  // 'Milka',
-  // 'Nike',
-  // 'Oreo',
-  // 'Ray-Ban',
-  // 'Samsung',
-  // 'Tommy Hilfiger',
-  // 'Vans'
+  {
+    name: 'H&M',
+    logo: hm,
+    link: 'https://www2.hm.com/en_us/index.html'
+  },
+  {
+    name: 'MAC',
+    logo: mac,
+    link: 'https://www.maccosmetics.com/'
+  },
+  {
+    name: 'Nike',
+    logo: nike,
+    link: 'https://www.nike.com/'
+  },
+  {
+    name: 'Oreo',
+    logo: oreo,
+    link: 'https://www.oreo.com/'
+  },
+  {
+    name: 'Nike',
+    logo: nike,
+    link: 'https://www.nike.com/'
+  },
+  {
+    name: 'Ray Ban',
+    logo: rayban,
+    link: 'https://www.ray-ban.com/'
+  },
+  {
+    name: 'Samsung',
+    logo: samsung,
+    link: 'https://www.samsung.com/us/'
+  },
+  {
+    name: 'Tommy Hilfiger',
+    logo: tommyhilfiger,
+    link: 'https://tommy.com/'
+  },
 ];
