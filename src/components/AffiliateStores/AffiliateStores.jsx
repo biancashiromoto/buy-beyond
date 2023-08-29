df0b6679e2ccb2123a9b71d5fc18d695d7fe897c
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-29 19:08:33 -0300

    style: Finish styling AffiliateStores

--- src/components/AffiliateStores/AffiliateStores.jsx
+++ src/components/AffiliateStores/AffiliateStores.jsx
@@ -1,27 +1,30 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { brands } from '../../helpers/brands';
+import './AffiliateStores.css';
 
 const AffiliateStores = () => {
-  const [stores, setStores] = useState(brands);
+  const [stores] = useState(brands);
   return (
-    <div id='affiliate-stores'>
-      <h3 className='text-center'>AffiliateStores</h3>
-      {stores.map((store, index) => {
-       return (
-        <a
-          key={index}
-          href={store.link}
-          target='_blank'
-          className={`store-link ${store}`} rel="noreferrer"
-        >
-          <img
-            className='w-25'
-            src={store.logo} 
-            alt={store.name}
-          />
-        </a>
-      )
-      })}
+    <div id='affiliate-stores' className='d-flex flex-column mx-4'>
+      <h3 className='text-center'>Lojas parceiras</h3>
+      <span className='text-center'>Explore as marcas que recomendamos para você</span>
+      <div className='brands-container'>
+        {stores.map((store, index) => {
+          return (
+            <a
+              key={index}
+              href={store.link}
+              target='_blank'
+              className={`store-link ${store.name}`} rel="noreferrer"
+            >
+              <img
+                src={store.logo} 
+                alt={store.name}
+              />
+            </a>
+          )
+          })}
+      </div>
     </div>
   )
 }
