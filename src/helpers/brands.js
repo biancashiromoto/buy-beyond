f37c419d7ac96690221596ed547bf3c47afeb24d
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-29 18:39:58 -0300

    chore: Delete unused files

---
+++ src/helpers/brands.js
@@ -0,0 +1,72 @@
+import adidas from '../assets/brands/adidas.png';
+import apple from '../assets/brands/apple.png';
+import calvinKlein from '../assets/brands/calvinklein.png';
+import dior from '../assets/brands/dior.png';
+import goPro from '../assets/brands/gopro.png';
+import haagenDazs from '../assets/brands/haagendazs.png';
+// import calvinKlein from '../assets/brands/calvinklein.png';
+// import calvinKlein from '../assets/brands/calvinklein.png';
+// import calvinKlein from '../assets/brands/calvinklein.png';
+// import calvinKlein from '../assets/brands/calvinklein.png';
+// import calvinKlein from '../assets/brands/calvinklein.png';
+// import calvinKlein from '../assets/brands/calvinklein.png';
+// import calvinKlein from '../assets/brands/calvinklein.png';
+// import calvinKlein from '../assets/brands/calvinklein.png';
+// import calvinKlein from '../assets/brands/calvinklein.png';
+
+export const brands = [
+  {
+    name: 'Adidas',
+    logo: adidas,
+    link: 'https://www.adidas.com/us'
+  },
+  {
+    name: 'Apple',
+    logo: apple,
+    link: 'https://www.apple.com/'
+  },
+  {
+    name: 'Calvin Klein',
+    logo: calvinKlein,
+    link: 'https://www.calvinklein.com/'
+  },
+  {
+    name: 'Dior',
+    logo: dior,
+    link: 'https://www.dior.com/en_us'
+  },
+  {
+    name: 'GoPro',
+    logo: goPro,
+    link: 'https://gopro.com/en/us//'
+  },
+  {
+    name: 'Häagen-Dazs',
+    logo: haagenDazs,
+    link: 'https://www.icecream.com/us/en/brands/haagen-dazs'
+  },
+  // {
+  //   name: 'Calvin Klein',
+  //   logo: calvinKlein,
+  //   link: 'https://www.calvinklein.com/'
+  // },
+  // {
+  //   name: 'Calvin Klein',
+  //   logo: calvinKlein,
+  //   link: 'https://www.calvinklein.com/'
+  // },
+  // 'Apple',
+  // 'Calvin Klein',
+  // 'Dior',
+  // 'GoPro',
+  // 'Häagen-Dazs',
+  // 'H&M',
+  // 'MAC',
+  // 'Milka',
+  // 'Nike',
+  // 'Oreo',
+  // 'Ray-Ban',
+  // 'Samsung',
+  // 'Tommy Hilfiger',
+  // 'Vans'
+];
