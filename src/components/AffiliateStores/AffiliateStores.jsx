633fb7ef31a29685465339ffff9ce76f8c2786a8
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-08-30 16:17:12 -0300

    style: Change h3 for h2

--- src/components/AffiliateStores/AffiliateStores.jsx
+++ src/components/AffiliateStores/AffiliateStores.jsx
@@ -6,7 +6,7 @@ const AffiliateStores = () => {
   const [stores] = useState(brands);
   return (
     <div id='affiliate-stores' className='d-flex flex-column mx-4'>
-      <h3 className='text-center'>Lojas parceiras</h3>
+      <h2 className='text-center'>Lojas parceiras</h2>
       <span className='text-center'>Explore as marcas que recomendamos para você</span>
       <div className='brands-container'>
         {stores.map((store, index) => {
